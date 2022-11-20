73ef0f2337aef26d8e18fd582c5349a1b93bb421
kanokpich <kanokpichchai29@gmail.com>
2022-11-20 15:38:35 +0700

    Update User.sol

--- contract/contracts/User.sol
+++ contract/contracts/User.sol
@@ -1,11 +1,10 @@
 pragma solidity ^0.8.17;
 
-import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 import "../node_modules/hardhat/console.sol";
 import "./Printer.sol";
 import "./Transaction.sol";
 
-contract User is Ownable {
+contract User {
     Printer public printerContract;
     Transaction public transactionContract;
     constructor() {}
